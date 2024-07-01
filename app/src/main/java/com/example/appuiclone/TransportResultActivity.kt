f968c999326742749013fc88414adc265535b09c
Jisung15 <gimseongjin302@gmail.com>
2024-07-01 14:35:56 +0900

    일단은 입력받아서 넘겨주고 출력해주는 것까지 함

--- app/src/main/java/com/example/appuiclone/TransportResultActivity.kt
+++ app/src/main/java/com/example/appuiclone/TransportResultActivity.kt
@@ -1,32 +1,35 @@
 package com.example.appuiclone
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.EditText
-import android.widget.LinearLayout
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ListView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class TransportResultActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_transport_result)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val transportButton = findViewById<LinearLayout>(R.id.button1)
-        val transportText = findViewById<EditText>(R.id.tv_language)
+        val value = findViewById<TextView>(R.id.tv_transport)
+        val finishButton = findViewById<Button>(R.id.btn_finish)
 
-        transportButton.setOnClickListener{
-            val intent = Intent(this, TransportResultActivity::class.java)
-            intent.putExtra("text", transportText.text.toString())
-            startActivity(intent)
+        val text = intent.getStringExtra("text")
+        value.text = "번역 결과\n\n한국어 : ${text}\n영어 : (대충 영어로 번역한 결과)"
+
+        finishButton.setOnClickListener {
+            finish()
         }
     }
 }
